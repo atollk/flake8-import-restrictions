d0061494a66f53ea4bdf312ce3a42ad243050b40
Andreas Tollk?tter <andreas.tollkoetter@rwth-aachen.de>
2024-06-27 20:52:19 +0200

    renamed test

--- tests/test_imports_submodule.py
+++ tests/test_imports_submodule.py
@@ -26,9 +26,6 @@ def test_relative_1():
     assert not imports_submodule(FILE1, 1, "resources.a.c", "C")
     assert imports_submodule(FILE2, 1, "a", "c")
     assert not imports_submodule(FILE2, 1, "a.c", "C")
-    # Check that the empty `from_` is handled correctly.
-    # For the complete code to run, we need to check a not-yet imported module.
-    assert not imports_submodule(FILE1, 1, "", "not_existing_module")
 
 
 def test_relative_2():
@@ -40,3 +37,7 @@ def test_relative_2():
 def test_relative_3():
     assert imports_submodule(FILE3, 3, "resources.a", "c")
     assert not imports_submodule(FILE3, 3, "resources.b", "B")
+
+
+def test_non_existant():
+    assert not imports_submodule(FILE1, 1, "a", "DOESNOTEXIST")
